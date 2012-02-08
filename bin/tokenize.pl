5a760dc714325dca81d6fac6a2bfc5882f1ee72a
Vaclav Petricek <vpetricek@eharmony.com>
2012-02-08 19:09:45 +0000

    simple tokenization routine

---
+++ bin/tokenize.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+sub tokenize ($)
+{
+  my ($a)  = @_;
+
+return map { $_ = lc $_; s/'$//; s/^'//; $_ }
+split /[^\w']+/, $a;
+
+}
+
+while(<>)
+{
+	print join " ", tokenize($_);
+}
