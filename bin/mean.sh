81952cf255966b69908cb2de9012f134377e01a3
Vaclav Petricek <vpetricek@eharmony.com>
2012-03-28 09:19:05 -0700

    refactor, +use sapply

--- bin/mean.sh
+++ bin/mean.sh
@@ -2,9 +2,23 @@
 
 cd $(dirname "$0" )
 
+usage="Usage: cat *.rocs | $0" 
+
+function rcode()
+{
+cat << __EOF__
+mystdin <- file("stdin")
+d <- read.table(mystdin)
+m=as.numeric(mean(d[,1]))
+s=sapply(d,sd)
+cat(m, s, '\n', sep = '\t')
+q(runLast=FALSE)
+__EOF__
+}
+
 if [ "$1" == "help" ]
 then
-	echo "Usage: cat *.rocs | $0" 
+	echo $usage
 	exit
 fi
 
@@ -15,6 +29,6 @@ then
 fi
 
 
-R --vanilla --slave -f mean.r
+R --vanilla --slave -f <( rcode )
 
 cd -
