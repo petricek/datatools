f729b6590820c4ec0e685a50e14e8e0138092844
Vaclav Petricek <vpetricek@eharmony.com>
2012-11-26 16:25:18 -0800

    usage fix

--- bin/subsample.pl
+++ bin/subsample.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-my $p = shift @ARGV or die "Usage: $0 prob_accept\n";
+my $p = shift @ARGV or die "Usage: $0 prob_accept [invert_results]\n";
 
 my $seed = shift @ARGV;
 $seed = (defined $seed) ? $seed : 7;
