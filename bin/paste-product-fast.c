e1b0d5b3108ad9814217a5bf60db329174905343
Vaclav Petricek <vpetricek@eharmony.com>
2013-04-02 23:42:48 -0700

    +sep, rewind

--- bin/paste-product-fast.c
+++ bin/paste-product-fast.c
@@ -13,23 +13,43 @@ ssize_t read;
 int main(int argc, char *argv[])
 {
 
+        char * usage = "%s file1 file2 [sep]\n";
+
         fA = fopen(argv[1], "r");
+        fB = fopen(argv[2], "r");
+
+        char * sep = argv[3];
+        if(sep == NULL)
+                sep = "";
 
         if (fA == NULL) 
+        {
+           printf(usage, argv[0]);
            exit(EXIT_FAILURE);
+        }
 
         while ((read = getline(&u, &len, fA)) != -1) 
         {
                 u[strlen(u)-1] = 0; // chop
-                fB = fopen(argv[2], "r");
+                rewind(fB);
                 if (fB == NULL) 
+                {
+                   printf(usage, argv[0]);
                    exit(EXIT_FAILURE);
+                }
                 while ((read = getline(&i, &len, fB)) != -1) 
                 {
-                        printf("%s%s", u, i);
+                        if(sep != NULL)
+                        {
+                                printf("%s%s%s", u, sep, i);
+                        }
+                        else
+                        {
+                                printf("%s%s", u, i);
+                        }
                 }
-                fclose(fB);
         }
+        fclose(fB);
         fclose(fA);
 
 
