a5e0b49dd1fc2e62dd7187bc8f9c1d492a8a55c3
Vaclav Petricek <v.petricek@gmail.com>
2014-02-17 06:05:58 +0000

    cute robust tsvdump from Vertica

---
+++ bin/run_dv.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $usage="$0 {-f file.sql|-c 'select * from table'}\n";
+
+die $usage if $#ARGV <= 0;
+
+my $command = join " ",
+                   "vsql -h " . $ENV{'VSQL_HOST'} . " -A -F'|||' ",
+                   (map { "'$_'" } @ARGV),
+                   "| perl -ne 'print \$l if defined (\$l); s/\\t/\\\\t/g; s/\\|\\|\\|/\\t/g; \$l=\$_; } { print STDERR \$l if defined (\$l);'";
+my $test_command = "vsql -h " . $ENV{'VSQL_HOST'} . " -c 'select 1;' > /dev/null 2>&1";
+
+#print STDERR $command . "\n";
+#print STDERR $test_command . "\n";
+
+printf STDERR "Connecting to Vertica\n";
+while(system($test_command) != 0)
+{
+	printf STDERR ".";
+	sleep(15);
+}
+
+
+# run netezza and output clean data.
+
+my $status = system ("$command");
+
+if ($status == -1) 
+  {
+    print "\t\t$0 failed: $!\n";
+    exit 1;
+  }
+
+exit 0;
